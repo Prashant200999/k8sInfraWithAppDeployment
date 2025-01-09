bb9dfd16e8237de96e6b90b04d4073a2254cfe33
Prashant200999 <160811688+Prashant200999@users.noreply.github.com>
2025-01-09 22:37:24 +0530

    Update delete.sh

--- application_manifest/delete.sh
+++ application_manifest/delete.sh
@@ -9,5 +9,8 @@ kubectl delete -f statefulsets.yml
 kubectl delete -f mysql-configmap.yml
 kubectl delete pvc mysql-data-mysql-0
 kubectl delete -f persistent-volume.yml
+kubectl delete -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/cloud/deploy.yaml
+
+
 
 echo "All resources have been deleted."
