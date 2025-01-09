5b27a8e4fcd1077d29165d0ef0043d58148ff42f
Prashant200999 <160811688+Prashant200999@users.noreply.github.com>
2025-01-09 22:18:17 +0530

    Update delete.sh

--- application_manifest/delete.sh
+++ application_manifest/delete.sh
@@ -6,8 +6,8 @@ kubectl delete -f frontend-deployment.yml
 kubectl delete -f mysql-service.yml
 kubectl delete -f mysql-secrets.yml
 kubectl delete -f statefulsets.yml
-kubectl delete -f persistent-volume.yml
 kubectl delete -f mysql-configmap.yml
 kubectl delete pvc mysql-data-mysql-0
+kubectl delete -f persistent-volume.yml
 
 echo "All resources have been deleted."
